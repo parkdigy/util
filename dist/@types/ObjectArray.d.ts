5992b1aceb0d4a3e0becdb99dd9138785ac28657
박영철 <parkdigy@gmail.com>
2024-03-19 09:27:20 +0900

    1.0.5

--- dist/@types/ObjectArray.d.ts
+++ dist/@types/ObjectArray.d.ts
@@ -1,10 +1,11 @@
+export type ValueOf<T> = T[keyof T];
 export type Dict<T = unknown> = {
     [k: string]: T;
 };
 export type Arr<T = unknown> = T[];
 export type IsObject<T> = T extends Record<string, unknown> ? true : false;
 export type IsArray<T> = T extends unknown[] ? true : false;
-export type ValueOf<T> = T[keyof T];
-export type Merge<T> = {
+export type ObjectMerge<T> = {
     [K in keyof T]: T[K];
 };
+export type ArrayMerge<A extends any[]> = A extends [infer T, ...infer R] ? T & ArrayMerge<R> : unknown;
--- src/@types/ObjectArray.ts
+++ src/@types/ObjectArray.ts
@@ -1,8 +1,10 @@
+export type ValueOf<T> = T[keyof T];
+
 export type Dict<T = unknown> = { [k: string]: T };
 export type Arr<T = unknown> = T[];
 
 export type IsObject<T> = T extends Record<string, unknown> ? true : false;
 export type IsArray<T> = T extends unknown[] ? true : false;
 
-export type ValueOf<T> = T[keyof T];
-export type Merge<T> = { [K in keyof T]: T[K] };
+export type ObjectMerge<T> = { [K in keyof T]: T[K] };
+export type ArrayMerge<A extends any[]> = A extends [infer T, ...infer R] ? T & ArrayMerge<R> : unknown;
