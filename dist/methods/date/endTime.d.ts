70f39e69073a3c9eb808682ea792447275ec3899
parkdigy <parkdigy@gmail.com>
2025-06-03 15:31:06 +0900

    1.0.27

--- dist/methods/date/endTime.d.ts
+++ dist/methods/date/endTime.d.ts
@@ -1,7 +1,8 @@
 /********************************************************************************************************************
- * 날짜의 시간을 23:59:59 로 변경해서 반환
+ * 날짜의 시간을 23:59:59.999 로 변경해서 반환
  * ******************************************************************************************************************/
 import { Dayjs } from 'dayjs';
-export declare function endTime<T extends Date | Dayjs | undefined>(dt?: T): T extends undefined ? Date : T;
+export declare function endTime(): Date;
+export declare function endTime<T extends Date | Dayjs>(dt?: T): T;
 export declare function endTime(dt: string, format?: string): Date;
 export default endTime;
--- src/methods/date/endTime.ts
+++ src/methods/date/endTime.ts
@@ -1,10 +1,11 @@
 /********************************************************************************************************************
- * 날짜의 시간을 23:59:59 로 변경해서 반환
+ * 날짜의 시간을 23:59:59.999 로 변경해서 반환
  * ******************************************************************************************************************/
 
 import dayjs, { Dayjs } from 'dayjs';
 
-export function endTime<T extends Date | Dayjs | undefined>(dt?: T): T extends undefined ? Date : T;
+export function endTime(): Date;
+export function endTime<T extends Date | Dayjs>(dt?: T): T;
 export function endTime(dt: string, format?: string): Date;
 export function endTime(dt?: Date | Dayjs | string, format?: string) {
   if (dt === undefined || dt instanceof Date || typeof dt === 'string') {
